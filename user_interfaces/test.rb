0fc975220cd2771d5cce83be13a24224bc250a3c
Matt Miller <mmiller@hick.org>
2005-05-18 04:43:45 +0000

    yup yup
    git-svn-id: file:///home/svn/incoming/trunk@2494 4d416f70-5f16-0410-b530-b9f4589650da

--- user_interfaces/test.rb
+++ user_interfaces/test.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby -I../Framework -I../Modules
 
 require 'Zsf/Framework'
-require 'Encoders/Generic/IA32/jmp_call_additive'
+require 'Encoders/Generic/IA32/JmpCallAdditive'
 require 'Nops/IA32/SingleByte'
 
 framework = Zsf::Framework.new
